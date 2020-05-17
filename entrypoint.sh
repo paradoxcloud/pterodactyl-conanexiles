f288cc0f9e8a3686516e3a40c2509a498c1504ca
arajdon <pawlus.patrick@gmail.com>
2020-05-17 17:47:20 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sleep 2
+
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+export WINEARCH=win64 
+export WINEPREFIX=/home/steam/.wine64
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' ${MODIFIED_STARTUP}
+
+if [ $? -ne 0 ]; then
+    echo "PTDL_CONTAINER_ERR: There was an error while attempting to run the start command."
+    exit 1
+fi
